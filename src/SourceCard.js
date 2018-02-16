091e0e788fe5f51b4938d7b4fd233e6b4561f50f
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-16 11:28:34 -0600

    subscribing works

--- src/SourceCard.js
+++ src/SourceCard.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './ui-toolkit/css/nm-cx/main.css'
 import './App.css';
+import { addSourceToSubscriptions } from './state/actions';
+import { connect } from 'react-redux';
 
 class SourceCard extends Component {
-
 
 
   render() {
@@ -11,10 +12,17 @@ class SourceCard extends Component {
       <div className="card sourceCard">
         <div>Name: {this.props.name}</div>
         <div>Description: {this.props.description}</div>
-        <button className={this.props.isSubscribed ? "warning" : "success"}>{this.props.isSubscribed ? "Subscribed" : "Subscribe"}</button>
+        <button onClick={() => this.props.addSourceToSubscriptions({sourceId: this.props.id})} disabled={this.props.isSubscribed} className={this.props.isSubscribed ? "warning" : "success"}>{this.props.isSubscribed ? "Subscribed" : "Subscribe"}</button>
       </div>
     );
   }
 }
 
-export default SourceCard;
+const mapDispatchToProps = dispatch => {
+  return {
+    addSourceToSubscriptions: (sourceObj) => dispatch(addSourceToSubscriptions(sourceObj)),
+  }
+};
+
+
+export default connect(null, mapDispatchToProps)(SourceCard)
