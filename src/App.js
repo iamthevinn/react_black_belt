c094e641a9e3da3c69bdcbff2fc152a5d3bc1d20
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-16 06:52:37 -0600

    clean slate to start with on final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ui-toolkit/css/nm-cx/main.css'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
