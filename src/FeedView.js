31c2cc0aa4153fd6982434c5a50a543d8ab3668f
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-16 11:48:32 -0600

    notes taken on how to get multiple news source headlines

--- src/FeedView.js
+++ src/FeedView.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import './ui-toolkit/css/nm-cx/main.css'
 import './App.css';
+import { loadSubscribedSourcesToState } from './state/actions';
+import { connect } from 'react-redux';
 
 class FeedView extends Component {
+
+  componentDidMount() {
+    this.props.loadSubscribedSourcesToState();
+  }
+
   render() {
-    return (
+      return (
         <div>
-        FeedView
+          <h1 className="pageHeader">
+            Personal Feed
+          </h1>
+            <div className="topHeadlinesContainer">
+              <div className="topHeadlinesInnerContainer">
+              
+            </div>
+          </div>
         </div>
-    );
+      );
   }
 }
 
-export default FeedView;
+const mapStateToProps = state => {
+
+  return {
+    subscribedHeadlines: state.subscribedHeadlines
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadSubscribedSourcesToState: () => dispatch(loadSubscribedSourcesToState())
+  }
+};
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(FeedView)
--- src/notes.md
+++ src/notes.md
@@ -22,5 +22,8 @@
 
 - Top Headlines - http://newsapi.org/v2/top-headlines?country=us&apiKey=9d5b1ad7e74e4d0bb6f3b13741235e56
 - Sources - http://newsapi.org/v2/sources?apiKey=9d5b1ad7e74e4d0bb6f3b13741235e56
-- /v2/everything
-- 
+- Everyting matching bloomberg http://newsapi.org/v2/everything?sources=bloomberg,fortune&language=en&pageSize=100&apiKey=9d5b1ad7e74e4d0bb6f3b13741235e56
+
+api call
+
+http://newsapi.org/v2/everything?sources= comma seperated strings by news source id &language=en&pageSize=100&apiKey=9d5b1ad7e74e4d0bb6f3b13741235e56
